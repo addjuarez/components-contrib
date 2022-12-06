800724736da5628c4d4aa84e737750d477e0ed6e
addjuarez <addiajuarez@gmail.com>
2022-12-06 15:30:28 -0600

    rest

---
+++ .github/infrastructure/terraform/conformance/pubsub/aws/snssqs/snssqs.tf
@@ -0,0 +1,9 @@
+variable "super_secret_variable" {
+    type = "string
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                        = "terraform-example-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+}
