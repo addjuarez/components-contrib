ba267702e4204ede1ebabdc5c5410ea632da8ecc
addjuarez <addiajuarez@gmail.com>
2022-12-07 21:04:49 -0600

    test

--- .github/infrastructure/terraform/conformance/pubsub/aws/snssqs/snssqs.tf
+++ .github/infrastructure/terraform/conformance/pubsub/aws/snssqs/snssqs.tf
@@ -1,5 +1,33 @@
-resource "aws_sqs_queue" "terraform_queue" {
-  name                        = "terraform-example-queue.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
+resource "aws_sns_topic" "testTopic" {
+  name = "testTopic"
+}
+
+resource "aws_sns_topic" "multiTopic1" {
+  name = "multiTopic1"
+}
+
+resource "aws_sns_topic" "multiTopic2" {
+  name = "multiTopic2"
+}
+
+resource "aws_sqs_queue" "testQueue" {
+  name = "testQueue"
+}
+
+resource "aws_sns_topic_subscription" "multiTopic1_testQueue" {
+  topic_arn = aws_sns_topic.multiTopic1.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.testQueue.arn
+}
+
+resource "aws_sns_topic_subscription" "multiTopic2_testQueue" {
+  topic_arn = aws_sns_topic.multiTopic1.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.testQueue.arn
+}
+
+resource "aws_sns_topic_subscription" "testTopic_testQueue" {
+  topic_arn = aws_sns_topic.multiTopic1.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.testQueue.arn
 }
