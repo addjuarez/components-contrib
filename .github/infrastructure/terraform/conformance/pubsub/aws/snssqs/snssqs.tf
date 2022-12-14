5ddad95fa5712e58503ae58e3cc6f5200e16ce28
addjuarez <6789375+addjuarez@users.noreply.github.com>
2022-12-14 19:08:42 +0000

    run_id
    destroy
    
    region
    
    test

--- .github/infrastructure/terraform/conformance/pubsub/aws/snssqs/snssqs.tf
+++ .github/infrastructure/terraform/conformance/pubsub/aws/snssqs/snssqs.tf
@@ -1,3 +1,19 @@
+variable "RUN_ID" {
+    type        = string
+    description = "This is an example input variable using env variables."
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_sns_topic" "testTopic123" {
+  name = "certification-pubsub-topic-active-${var.RUN_ID}"
+  tags = {
+    dapr-topic-name = "certification-pubsub-topic-active-${var.RUN_ID}"
+  }
+}
+
 resource "aws_sns_topic" "testTopic" {
   name = "testTopic"
   tags = {
@@ -51,44 +67,24 @@ resource "aws_sqs_queue_policy" "testQueue_policy" {
 {
   "Version": "2012-10-17",
   "Id": "sqspolicy",
-  "Statement": [
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.testQueue.arn}",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "${aws_sns_topic.testTopic.arn}"
-        }
-      }
-    }, 
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.testQueue.arn}",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "${aws_sns_topic.multiTopic1.arn}"
-        }
-      }
+  "Statement": [{
+    "Sid": "Allow-SNS-SendMessage",
+    "Effect": "Allow",
+    "Principal": {
+      "Service": "sns.amazonaws.com"
     },
-    {
-      "Sid": "First",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "sqs:SendMessage",
-      "Resource": "${aws_sqs_queue.testQueue.arn}",
-      "Condition": {
-        "ArnEquals": {
-          "aws:SourceArn": "${aws_sns_topic.multiTopic2.arn}"
-        }
+    "Action": "sqs:SendMessage",
+    "Resource": "${aws_sqs_queue.testQueue.arn}",
+    "Condition": {
+      "ArnEquals": {
+        "aws:SourceArn": [
+          "${aws_sns_topic.testTopic.arn}",
+          "${aws_sns_topic.multiTopic1.arn}",
+          "${aws_sns_topic.multiTopic2.arn}"
+        ]
       }
     }
-  ]
+  }]
 }
 POLICY
 }
